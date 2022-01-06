8385641e8eff33101a8b285ba4bf7e64dfdf16e0
Nerdmaster <brody76@gmail.com>
2022-01-06 13:46:21 -0800

    Fix entrypoint when command is empty

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 
 # If there's no command, we just run the server
 if [[ "${1:0:1}" == '' ]]; then
-  set -- dynamo-core "$@"
+  set -- dynamo-core "-rpcuser=$RPC_USER" "-rpcpassword=$RPC_PASS" "-datadir=/dynamo/data" "-conf=/dynamo/dynamo.conf" "-nodebuglogfile" "$@"
 fi
 
 # If the command starts with a hyphen, prepend dynamo-core and add necessary flags
